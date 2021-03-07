8c28949eb3f39be762d6dcf3a147837d80c31619
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-03-07 00:12:04 -0800

    4 more regions

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -5,7 +5,7 @@ import { Application } from '../lib/app';
 import { Database } from '../lib/db';
 const app = new cdk.App();
 
-new Database(app, "BackendDBMulti", "MultiRegionAppDB", ["us-east-1"], {
+new Database(app, "BackendDBMulti", "MultiRegionAppDB", ["us-east-1", "us-east-2", "us-west-1", "eu-west-1", "eu-west-2"], {
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION
@@ -23,5 +23,33 @@ const appeast = new Application(app, "ApplicationMultiEast", "MultiRegionAppDB",
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: "us-east-1"
+    }
+})
+
+const appwest1 = new Application(app, "ApplicationMultiWest1", "MultiRegionAppDB", "multiapp.ihatemyself.xyz", "ihatemyself.xyz", "Z0282398R4BTQCQCYHSP", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "us-west-1"
+    }
+})
+
+const appeu1 = new Application(app, "ApplicationMultiEU1", "MultiRegionAppDB", "multiapp.ihatemyself.xyz", "ihatemyself.xyz", "Z0282398R4BTQCQCYHSP", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "eu-west-1"
+    }
+})
+
+const appeu2 = new Application(app, "ApplicationMultiEU2", "MultiRegionAppDB", "multiapp.ihatemyself.xyz", "ihatemyself.xyz", "Z0282398R4BTQCQCYHSP", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "eu-west-2"
+    }
+})
+
+const appeast2 = new Application(app, "ApplicationMultiEast2", "MultiRegionAppDB", "multiapp.ihatemyself.xyz", "ihatemyself.xyz", "Z0282398R4BTQCQCYHSP", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: "us-east-2"
     }
 })
